b2d4bf97b7e00ca6aba0464a0f1aeab309567dad
lamiru <lamiru@gmail.com>
2016-08-30 20:25:26 +0900

    add static files

--- reactor/settings.py
+++ reactor/settings.py
@@ -120,3 +120,18 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# Compressor
+
+COMPRESS_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+    'compressor.finders.CompressorFinder',
+)
+
+COMPRESS_PRECOMPILERS = (
+    ('text/x-sass', 'django_libsass.SassCompiler'),
+)
