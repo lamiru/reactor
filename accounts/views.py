f267f063eb6086d85b3d601d9ca1e22aa1e25d03
lamiru <lamiru@gmail.com>
2016-08-31 08:13:28 +0900

    add authentication feature

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,35 @@
+from django.contrib.auth import login as auth_login
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import AuthenticationForm
+from django.core.urlresolvers import reverse_lazy
 from django.shortcuts import render
+from django.views.generic import FormView
 
 
+class LoginView(FormView):
+    template_name = '_form.html'
+    form_class = AuthenticationForm
+
+    def get_success_url(self):
+       next_url = self.request.POST.get('next')
+       if next_url:
+           return '%s' % (next_url)
+       else :
+           return reverse_lazy('accounts:index')
+
+    def get_context_data(self, **kwargs):
+        context = super(LoginView, self).get_context_data(**kwargs)
+        context['submit'] = 'Log in'
+        context['next'] = self.request.GET.get('next') and self.request.GET.get('next') or ''
+        return context
+
+    def form_valid(self, form):
+        user = form.get_user()
+        auth_login(self.request, user)
+        return super(LoginView, self).form_valid(form)
+
+
+@login_required
 def index(request):
     return render(request, 'accounts/index.html', {
     })
