0a6aaa6be8338b260cc3f5a725d1ee9674b34404
lamiru <lamiru@gmail.com>
2016-09-10 17:25:21 +0900

    add reactions detail

--- reactions/urls.py
+++ reactions/urls.py
@@ -4,4 +4,5 @@ from .views import *
 app_name = 'reactions'
 
 urlpatterns = [
+    url(r'^(?P<pk>\d+)/$', detail, name='detail'),
 ]
--- reactions/views.py
+++ reactions/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, get_object_or_404
+from .models import Reaction
 
-# Create your views here.
+
+@login_required
+def detail(request, pk):
+    current_reaction = get_object_or_404(Reaction, pk=pk)
+    reaction = current_reaction
+    reaction_list = []
+    if reaction.target:
+        reaction_list.append(reaction.target)
+    for i in reaction_list:
+        reaction = Reaction.objects.get(pk=i.pk)
+        if reaction.target:
+            reaction_list.append(reaction.target)
+    reaction_list.insert(0, current_reaction)
+    return render(request, 'reactions/detail.html', {
+        'reaction_list': reaction_list,
+    })
