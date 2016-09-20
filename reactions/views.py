b3ba23690dbf51b6056a4e0cf0254bc75d7421e0
lamiru <lamiru@gmail.com>
2016-09-20 16:53:25 +0900

    improve SQL logic in reactions detail

--- reactions/views.py
+++ reactions/views.py
@@ -5,16 +5,15 @@ from .models import Reaction
 
 @login_required
 def detail(request, pk):
-    current_reaction = get_object_or_404(Reaction, pk=pk)
-    reaction = current_reaction
+    reaction = get_object_or_404(Reaction, pk=pk)
     reaction_list = []
-    if reaction.target:
-        reaction_list.append(reaction.target)
-    for i in reaction_list:
-        reaction = Reaction.objects.get(pk=i.pk)
-        if reaction.target:
-            reaction_list.append(reaction.target)
-    reaction_list.insert(0, current_reaction)
+    reaction_list.append(reaction)
+
+    for reaction in reaction_list:
+        if reaction.target is not None:
+            reaction = reaction.target
+            reaction_list.append(reaction)
+
     return render(request, 'reactions/detail.html', {
         'reaction_list': reaction_list,
     })
