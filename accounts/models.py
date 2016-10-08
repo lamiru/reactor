3e5b0d504d5fcc081f3688dac050d4577dd717c9
lamiru <lamiru@gmail.com>
2016-10-08 19:43:34 +0900

    add accounts user profile model

--- accounts/models.py
+++ accounts/models.py
@@ -1,1 +1,13 @@
 from django.db import models
+from django.db.models.signals import pre_save
+from django.conf import settings
+from helpers.file import random_name_with_file_field
+
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL)
+    photo = models.ImageField(
+        blank=True, null=True,
+        upload_to=random_name_with_file_field
+    )
+    about = models.TextField(blank=True, null=True)
