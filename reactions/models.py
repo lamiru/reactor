8dbdf3d7ef624c804f58d8e8fe64c0a218d27d6f
lamiru <lamiru@gmail.com>
2016-10-10 20:35:03 +0900

    simplify reactions detail

--- reactions/models.py
+++ reactions/models.py
@@ -14,6 +14,33 @@ class Reaction(models.Model):
     def __str__(self):
         return str(self.id)
 
+    def get_tree(self):
+        reaction_tree = []
+        reaction_tree.append(self)
+        for reaction in reaction_tree:
+            if reaction.target is not None:
+                reaction = reaction.target
+                reaction_tree.append(reaction)
+        return reaction_tree
+
+    def get_brothers(self):
+        if self.target is not None:
+            return self.target.target_reactions.all()
+        else:
+            return Reaction.objects.filter(target=None)
+
+    def get_parents(self):
+        if self.target is not None:
+            return self.target.get_brothers()
+        else:
+            return None
+
+    def get_children(self):
+        return self.target_reactions.all()
+
+    def get_family(self):
+        return Reaction.objects.filter(topic=self.topic)
+
 
 class Rate(models.Model):
     rator = models.ForeignKey(settings.AUTH_USER_MODEL, db_index=True)
--- reactions/views.py
+++ reactions/views.py
@@ -6,28 +6,18 @@ from .models import Reaction
 @login_required
 def detail(request, pk):
     reaction = get_object_or_404(Reaction, pk=pk)
-    list_3 = reaction.target_reactions.all()
-    reaction_tree = []
-    reaction_tree.append(reaction)
 
-    for reaction in reaction_tree:
-        if reaction.target is not None:
-            reaction = reaction.target
-            reaction_tree.append(reaction)
+    tree = reaction.get_tree()
+    children = reaction.get_children()
+    brothers = reaction.get_brothers()
+    parents = reaction.get_parents()
 
-    try:
-        list_2 = reaction_tree[1].target_reactions.all()
-    except:
-        list_2 = None
-    try:
-        list_1 = reaction_tree[2].target_reactions.all()
-    except:
-        list_1 = None
-    print(reaction_tree)
+    if parents is None:
+        lists = [brothers, children, [],]
+    else:
+        lists = [parents, brothers, children,]
 
     return render(request, 'reactions/detail.html', {
-        'reaction_tree': reaction_tree,
-        'list_3': list_3,
-        'list_2': list_2,
-        'list_1': list_1,
+        'tree': tree,
+        'lists': lists,
     })
