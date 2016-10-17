ff8940efb45f8e4b913059f7fbd766f89cce8ff5
lamiru <lamiru@gmail.com>
2016-10-17 14:41:26 +0900

    disable delete feature

--- reactions/urls.py
+++ reactions/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     url(r'^(?P<pk>\d+)$', detail, name='detail'),
     url(r'^(?P<pk>\d+)/good$', rate_good, name='good'),
     url(r'^(?P<pk>\d+)/pass$', rate_pass, name='pass'),
-    url(r'^(?P<pk>\d+)/delete$', delete, name='delete'),
+    # url(r'^(?P<pk>\d+)/delete$', delete, name='delete'),
     url(r'^(?P<pk>\d+)/reaction/new$', reaction_new, name='reaction_new'),
 ]
