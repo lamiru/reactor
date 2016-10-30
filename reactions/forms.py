86ea7262b8050589cba9b7033cb260579f43e554
lamiru <lamiru@gmail.com>
2016-10-30 13:44:14 +0900

    add thumbnail feature - step1

--- reactions/forms.py
+++ reactions/forms.py
@@ -5,4 +5,7 @@ from .models import Reaction
 class ReactionForm(forms.ModelForm):
     class Meta:
         model = Reaction
-        fields = ('title', 'contents',)
+        fields = (
+            'title', 'contents',
+            'url', 'url_title', 'url_description', 'url_image'
+        )
