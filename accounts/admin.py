e9ae3f22ebae8eca620b819444e8362b3c99f681
lamiru <lamiru@gmail.com>
2016-11-03 13:30:50 +0900

    add accounts user_profile photo_default

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = (
+        'id', 'user', 'photo', 'about',
+        'created_at', 'updated_at', 
+    )
+
+admin.site.register(UserProfile, UserProfileAdmin)
--- accounts/models.py
+++ accounts/models.py
@@ -12,3 +12,42 @@ class UserProfile(models.Model):
         upload_to=random_name_with_file_field
     )
     about = models.TextField(max_length=2000, blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return str(self.id)
+
+    def photo_letters(self):
+        return self.user.username[:2]
+
+    def photo_bg_color(self):
+        color_list = [
+            'CCCCCC',
+            'FFAA00',
+            '00FFAA',
+            'AA00FF',
+            'AAFF00',
+            '00AAFF',
+            'FF00AA',
+            '9900BB',
+            'BB9900',
+            '00BB99',
+            '333333',
+            '0033CC',
+            'CC0033',
+            '33CC00',
+            '3300CC',
+            'CC3300',
+            '00CC33',
+            '660099',
+            '996600',
+            '009966',
+        ]
+        return color_list[self.id % 20]
+
+    def photo_default(self):
+        return 'http://ipsumimage.appspot.com/140x45,{0}?l=l%20%20%20{1}%20%20%20l&f=ffffff'.format(
+            self.photo_bg_color(),
+            self.photo_letters(),
+        )
